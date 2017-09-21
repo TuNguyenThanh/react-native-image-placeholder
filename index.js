c1d5c01df995ba2719b79bc3016f480f8815e686
John.Yu <jungleyu@163.com>
2017-09-21 16:44:36 +0800

    PropTypes has been moved to a separate package.
    React.PropTypes is no longer supported.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { Image, ImageBackground, ActivityIndicator, View } from 'react-native';
 
 class ImageLoad extends React.Component {
